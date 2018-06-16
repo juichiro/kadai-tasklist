c91f5123b48a820fdfd3e620bf380702973fa044
juichi <0805yoshitomi@gmail.com>
2018-06-16 14:38:17 +0000

    revise update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     
-    if @task.save(task_params)
+    if @task.update(task_params)
       flash[:succes] = 'Taskは正常に更新されました'
       redirect_to @task
     else
